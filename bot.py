69aebe09ba1b4bf27cac86adbdf5e6f50d7c30ad
Anthony Keelan <anthony.keelan@gmail.com>
2018-10-04 17:17:17 -0600

    enabled logging

--- bot.py
+++ bot.py
@@ -8,6 +8,16 @@ from league import League, API_KEY
 import pycurl
 import certifi
 import asyncio
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
